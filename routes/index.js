c5979ecbe2de84efd2877689678dba2129b58ffc
Emmett <emmettaraymond@gmail.com>
2014-11-01 14:21:33 -0600

    Adding project to repository.

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET helloworld. */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', {title: 'Hello World'});
+});
+
+/* GET User List*/
+router.get('/userlist', function(req,res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      "userlist":docs
+    });
+  });
+});
+
+module.exports = router;
